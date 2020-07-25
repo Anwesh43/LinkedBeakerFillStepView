5406e5c69eba6feb04e7005886ffff441bf4203e
Anwesh43 <anweshthecool0@gmail.com>
2020-07-26 01:43:21 +0530

    adding BFSNode class

--- beakerfillstepview/src/main/java/com/anwesh/uiprojects/beakerfillstepview/BeakerFillStepView.kt
+++ beakerfillstepview/src/main/java/com/anwesh/uiprojects/beakerfillstepview/BeakerFillStepView.kt
@@ -96,4 +96,45 @@ class BeakerFillStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BFSNode(var i : Int, val state : State = State()) {
+
+        private var next : BFSNode? = null
+        private var prev : BFSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BFSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBFSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BFSNode {
+            var curr : BFSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
