d844cebe171e7eb3bcdc20c41b63d557d2bc27ba
Anwesh43 <anweshthecool0@gmail.com>
2020-07-26 01:35:15 +0530

    adding Canvas extension method

--- beakerfillstepview/src/main/java/com/anwesh/uiprojects/beakerfillstepview/BeakerFillStepView.kt
+++ beakerfillstepview/src/main/java/com/anwesh/uiprojects/beakerfillstepview/BeakerFillStepView.kt
@@ -28,3 +28,36 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 
+fun Canvas.drawBeakerFillStep(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    val sw : Float = w / sizeFactor
+    val sh : Float = h / sizeFactor
+    save()
+    translate(w / 2 - sw * 0.5f, h / 2 - sh * 0.5f)
+    for (j in 0..1) {
+        val sfj : Float = sf.divideScale(j * 2, parts)
+        val sj : Float = 1f - 2 * j
+        save()
+        translate(sw * j, sh * j)
+        drawLine(0f, 0f, 0f, sh * sfj * sj, paint)
+        restore()
+    }
+    save()
+    translate(0f, sh)
+    drawLine(0f, 0f, sw * sf2, 0f, paint)
+    paint.color = waterColor
+    drawRect(RectF(-sh * sf4, 0f, sw, sh * sf4), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawBFSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBeakerFillStep(scale, w, h, paint)
+}
